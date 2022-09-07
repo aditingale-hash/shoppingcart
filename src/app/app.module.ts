7ecfc20c8e3bfb51c001afa57da504547b1ac28c
aditingale-hash <aditiingale2000@gmail.com>
2022-09-07 22:34:59 +0530

    angular project with navbar routed with bootstraph and nodejs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarHomeComponent } from './component/navbar/navbar-home/navbar-home.component';
+import { ProfileHomeComponent } from './component/profile/profile-home/profile-home.component';
+import { CartHomeComponent } from './component/cart/cart-home/cart-home.component';
+import { OrderHomeComponent } from './component/order/order-home/order-home.component';
+import { ProductHomeComponent } from './component/product/product-home/product-home.component';
+import { WalletHomeComponent } from './component/wallet/wallet-home/wallet-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarHomeComponent,
+    ProfileHomeComponent,
+    CartHomeComponent,
+    OrderHomeComponent,
+    ProductHomeComponent,
+    WalletHomeComponent,
+
   ],
   imports: [
     BrowserModule,
